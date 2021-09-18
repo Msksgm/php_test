a4a1f0c7c34439a09faeb8af68438af870d8f6b9
Msksgm <msksgm3124@gmail.com>
2021-09-18 17:01:19 +0900

    add: phpの配列

--- index.php
+++ index.php
@@ -1,10 +1,20 @@
 <?php
 
-// 定数
+// 配列
 
-const MAX = 'TEST';
 
-var_dump(MAX);
+$array = [1, 2, 3];
 
+$array_2 = [
+    ['赤', '青', '黄'],
+    ['赤', '青', '黄'],
+];
+
+
+echo '<pre>';
+var_dump($array_2);
+echo '</pre>';
+
+echo $array_2[1][1];
 
 ?>
