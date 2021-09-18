822edb28891b2c7dbea25e1f26f44a50731b6196
Msksgm <msksgm3124@gmail.com>
2021-09-18 18:56:00 +0900

    add: 自作関数

--- index.php
+++ index.php
@@ -82,5 +82,19 @@ echo '<pre>';
 var_dump($array);
 echo '</pre>';
 
+$postalCode = '123-4567';
+
+function checkPostalCode($str){
+    $replaced = str_replace('-', '', $str);
+    $length = strlen($replaced);
+
+    if ($length === 7) {
+        return true;
+    }
+    return false;
+}
+
+var_dump(checkPostalCode($postalCode));
+
 ?>
 
