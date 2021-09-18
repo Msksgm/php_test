58470286c0f567113fdf04ea79347f9f1a9712c6
Msksgm <msksgm3124@gmail.com>
2021-09-18 17:08:57 +0900

    add: 演算子

--- index.php
+++ index.php
@@ -1,45 +1,14 @@
 <?php
 
-// 配列
+// 演算子
 
 
-$array = [1, 2, 3];
+$test_1 = 2;
+$test_2 = 3;
 
-$array_2 = [
-    ['赤', '青', '黄'],
-    ['赤', '青', '黄'],
-];
+$test_3 = $test_1 + $test_2;
 
-
-echo '<pre>';
-var_dump($array_2);
-echo '</pre>';
-
-echo $array_2[1][1];
-
-$array_member = [
-    'name' => '本田',
-    'height' => 170,
-    'hobby' => 'サッカー'
-];
-
-echo $array_member['hobby'];
-
-$array_member_2 = [
-    '本田' => [
-        'height' => 170,
-        'hobby' => 'サッカー'
-    ],
-    '香川' => [
-        'height' => 185,
-        'hobby' => 'サッカー'
-    ]
-];
-
-echo '<pre>';
-var_dump($array_member_2);
-echo '</pre>';
-
+echo $test_3;
 
 
 ?>
