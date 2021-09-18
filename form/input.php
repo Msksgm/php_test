d83acb4c79ff2f707f848ee1b3e3ac242f7af4bb
Msksgm <msksgm3124@gmail.com>
2021-09-18 19:10:50 +0900

    add: get通信を追加

---
+++ form/input.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<form metho="GET" action="input.php">
+氏名
+<input type="text" name="your_name"/>
+
+<input type="submit" value="送信" />
+</form>
+</body>
+</html>
