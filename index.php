9ebabb5c6e2666fc6fe50a118721ee519ceed36c
Msksgm <msksgm3124@gmail.com>
2021-09-18 18:41:38 +0900

    関数1

--- index.php
+++ index.php
@@ -36,5 +36,19 @@ $total = sumPrice(2, 5);
 
 echo $total;
 
+$str = 'abc';
+
+echo strlen($str);
+
+$text = 'あいうえお';
+
+echo mb_strlen($text);
+
+// 文字列の置換
+
+$str = '文字列を置換します';
+
+echo str_replace('置換', 'ちかん', $str);
+
 ?>
 
