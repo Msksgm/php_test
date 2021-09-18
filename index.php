2caecc41b4d926ae36270e94f12866813485f338
Msksgm <msksgm3124@gmail.com>
2021-09-18 18:50:22 +0900

    add: 文字列関数

--- index.php
+++ index.php
@@ -50,5 +50,25 @@ $str = '文字列を置換します';
 
 echo str_replace('置換', 'ちかん', $str);
 
+// 指定文字列で分割
+
+$str_2 = '指定文字列で、分割します';
+
+echo '<pre>';
+var_dump(explode('、', $str_2));
+echo '</pre>';
+
+// imploade
+
+// 正規表現
+
+$str_3 = '特定の文字列が含まれるか確認する';
+
+echo preg_match('/文字列/', $str_3);
+
+// 指定文字列から文字列を取得する
+
+echo mb_substr('あいう', 2);
+
 ?>
 
